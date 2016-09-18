3bf82cfb7cf41ba9882d65e49400ecc648e620cc
Pham Tuan Anh <tuananh200898@gmail.com>
2016-09-18 11:51:51 +0700

    Doing : Reformat Code & Use Typescript

--- admin/manage/brand.js
+++ admin/manage/brand.js
@@ -19,4 +19,29 @@ aptBrandModule.component('brand', {
             self.brand = new Brand();
         };
     }]
-});
+}).directive('fileImage', ['$http', '$location', '$parse', function ($http, $location) {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    var imagePath = element[0].files[0];
+                    if (typeof imagePath != 'undefined') {
+                        var fd = new FormData();
+                        fd.append('file', imagePath);
+                        $http.post('../brand/upload', fd, {
+                            transformRequest: angular.identity,
+                            headers: {'Content-Type': undefined}
+                        }).then(function (response) {
+                            if (response.data.success) {
+                                scope.srcImg = $location.protocol() + '://' + $location.host() + '/' + response.data.srcImage;
+                            } else {
+                                scope.imageUploadStatus = 'Upload image fail';
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    };
+}]);;
